0b152a441efa0eccf4d6d2b4a76c5b904030b4a3
Eric Kwok <ngaitung.kwok@u.nus.edu>
2019-09-04 18:09:23 +0800

    modified device1 device2 and ee5111_libraries

--- ee5111_libraries.py
+++ ee5111_libraries.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 from datetime import datetime as dt
 
-def csv_processing(filename,engine_number):
+def csv_processing(filename):
     engine_d = pd.read_csv(filename,delimiter=' |\n',header=None,engine='python')
     # To read the engine data
 
@@ -17,15 +17,10 @@ def csv_processing(filename,engine_number):
     # Initialize the headers
 
     engine_d.columns = Header # Insert headers for the data
-    ID = ['FD00'+str(engine_number)+'_'+str(id) for id in range(1,len(engine_d.index)+1)]
+    ID = [filename[6:11]+'_'+str(id) for id in range(1,len(engine_d.index)+1)]
     engine_d['ID'] = ID # Change the ID column
 
     engine_d['Timestamp'] = str(np.nan)
     engine_d['Matric Number'] = 'A0108484B'
 
     return engine_d
-
-eng_d1 = csv_processing('train_FD001.txt',1)
-now = dt.utcnow()
-eng_d1.at[1,'Timestamp'] = str(now)
-eng_d1.iloc[1]['Timestamp']
